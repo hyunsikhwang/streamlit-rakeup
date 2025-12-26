27e5aa2c74fa71e6b8571fc8aeb865c82dac0782
Hyunsik Hwang <gomting@naver.com>
2025-12-26 18:47:16 +0900

    benecafe

--- streamlit_app.py
+++ streamlit_app.py
@@ -491,7 +491,7 @@ chosen_id = stx.tab_bar(data=[
     stx.TabBarItemData(id=2, title="HIRA", description="with Requests"),
     stx.TabBarItemData(id=3, title="KOFIABOND", description="with Playwright"),
     # stx.TabBarItemData(id=4, title="복권", description="with Playwright"),
-    # stx.TabBarItemData(id=5, title="복리후생", description="with Playwright"),
+    stx.TabBarItemData(id=5, title="복리후생", description="with Playwright"),
     # stx.TabBarItemData(id=6, title="tmoney", description="with Playwright"),
 ], default=1)
 
